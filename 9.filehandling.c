fabd23bb96f98b7433b1dc1a0f77ba33a474cec8
Abhijit-co <158506755+Abhijit-co@users.noreply.github.com>
2025-08-21 19:26:44 +0530

    Add files via upload

---
+++ 9.filehandling.c
@@ -0,0 +1,53 @@
+//FILE HANDLING -STORE DATA IN TO THE TEXT FILE PERMANENTLY.
+
+//MODE= r w r+ w+ a
+//#include<stdio.h>
+//void main(){
+//	FILE *fp;
+//	fp=fopen("virat.txt","w");
+//	printf("\nMy name is:Abhijit");
+//	fprintf(fp,"\nMy name is:Abhijit");
+//}
+
+
+//#include<stdio.h>
+//void main(){
+//	FILE *fp;
+//	fp=fopen("Rohit.txt","r");
+//	int age;
+//    fscanf(fp,"%d",&age);
+//    printf("\nAge is:%d",age);
+//}
+
+
+//#include<stdio.h>
+//void main(){
+//	FILE *fp;
+//	fp=fopen("Dhoni.txt","r");
+//	int a,b,c;
+//	fscanf(fp,"%d%d",&a,&b);
+//	c=a+b;
+//	printf("Addition is %d",c);
+//}
+
+
+//
+//#include<stdio.h>
+//void main(){
+//	FILE *fp;
+//	fp=fopen("Sachin.txt","w");
+//	char ch ='A';
+//	fputc(ch,fp);
+//	printf("Record saved! Please Check");
+//}
+
+
+//
+//#include<stdio.h>
+//void main(){
+//	FILE *fp;
+//	fp=fopen("Sagar.txt","r");
+//	char ch ;
+//	ch = fgetc(fp);
+//	printf("%c",ch);
+//}
